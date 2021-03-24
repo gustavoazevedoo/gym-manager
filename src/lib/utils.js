a04915864f036898d24645951dfed7b2aa8fa62d
Gustavo Azevedo <gustavosazevedo@gmail.com>
2021-03-24 16:05:26 -0300

    config for database

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-  age: function (timestamp) {
+  age(timestamp) {
     const today = new Date();
     const birthDate = new Date(timestamp)
 
@@ -11,7 +11,7 @@ module.exports = {
     }
     return age
   },
-  date:(timestamp) => {
+  date(timestamp) {
     const birth = new Date(timestamp)
 
     const year = birth.getUTCFullYear()
